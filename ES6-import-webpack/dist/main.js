37810cc5da34438ad54e2990c318fc233730680c
nijunjie <nijunjie@corp.netease.com>
2020-11-02 00:21:58 +0800

    update js

--- ES6-import-webpack/dist/main.js
+++ ES6-import-webpack/dist/main.js
@@ -1,1 +1,21 @@
-(()=>{"use strict";console.log("b");const o={speak:function(){console.log("Hello World")}};console.log("c");const l={say:function(){console.log("Wow")}};console.log("a"),console.log("a call");const s={speak:o.speak,say:l.say};console.log("index"),console.log("index call"),s.speak()})();
+(() => {
+  "use strict";
+  console.log("b");
+  const o = {
+    speak: function () {
+      console.log("Hello World")
+    }
+  };
+  console.log("c");
+  const l = {
+    say: function () {
+      console.log("Wow")
+    }
+  };
+  console.log("a"), console.log("a call");
+  const s = {
+    speak: o.speak,
+    say: l.say
+  };
+  console.log("index"), console.log("index call"), s.speak()
+})();
